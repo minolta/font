104c8fd323538db2e17b1c8b4a1addb092892656
Kritchai Phromros <ky@pixka.me>
2024-03-01 19:44:24 +0700

    update

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  host: 'http://192.168.88.5:3333',
-  version: '0.0.1',
+  host: 'http://192.168.88.5:8080',
+  version: 'L0.0.2',
   fwhost:'htpp://192.168.88.5:2005'
 };
