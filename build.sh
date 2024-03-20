754efa4dcbf07bcd43464169881c54a20ae25216
Kritchai Phromros <ky@pixka.me>
2024-03-20 08:30:34 +0700

    update

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
 ng b;docker build -t kykub/pinfont:17 .;docker push  kykub/pinfont:17
+ng b -c jktest;docker build -t kykub/pinfont:jktest .;docker push  kykub/pinfont:jktest
+ng b -c local;docker build -t kykub/pinfont:jktestlocal .;docker push  kykub/pinfont:jktestlocal
